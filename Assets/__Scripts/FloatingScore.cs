39332abd5c6e2ac360ebba2f901aa87de5db1a89
CrimsonBlade <rutz.joe@gmail.com>
2016-04-08 23:31:07 -0400

    Floating Score
    Started floating score script.  game runs with no errors as of this
    version just not quite complete yet.

---
+++ Assets/__Scripts/FloatingScore.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum FSState{
+	idle,
+	pre,
+	active,
+	post
+}
+
+public class FloatingScore : MonoBehaviour {
+	public FSState		state = FSState.idle;
+	[Serialize Field]
+	private internal	_score = 0;
+	public string		scoreString;
+
+	public int score{
+		get{
+			return(_score);
+		}
+		set{
+			_score = value;
+			scoreString = Utils.AddCommasToNumber(_score);
+			GetComponent<GUIText>().text = scoreString;
+		}
+	}
+
+	//!!!!!!!!!!!!!!!!  START HERE  !!!!!!!!!!!!!!!!!!
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
